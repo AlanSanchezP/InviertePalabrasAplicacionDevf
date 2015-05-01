044f446f2714f5d8d8ce310ebf5397a7eb4a3f5e
Alan Sanchez <sanchez2597asp@outlook.com>
2015-04-30 21:48:17 -0500

    Agregada funcion invertirCadena

---
+++ aplicacionDevf.js
@@ -0,0 +1,12 @@
+function invertirCadena (cadena) {
+	var posicion = cadena.length - 1;
+	var nuevaCadena = "";
+	while (posicion >= 0) {
+    	nuevaCadena += cadena.charAt(posicion);
+    	posicion--;
+    }
+	alert("La oracion invertida es:" + nuevaCadena);
+  	alert("La letra e aparece " + (cadena.split("e").length - 1) + " veces.");
+}
+
+invertirCadena(prompt("Introduce la frase que invertire."));
