64dd6eb78a22a1dcd04de7b327f711dcc3821d07
Alan Sánchez Pineda <sanchez2597asp@outlook.com>
2015-05-24 00:00:57 -0500

    Update aplicacionDevf.js

--- aplicacionDevf.js
+++ aplicacionDevf.js
@@ -2,11 +2,11 @@ function invertirCadena (cadena) {
 	var posicion = cadena.length - 1;
 	var nuevaCadena = "";
 	while (posicion >= 0) {
-    	nuevaCadena += cadena.charAt(posicion);
-    	posicion--;
-    }
+		nuevaCadena += cadena.charAt(posicion);
+		posicion--;
+	}
 	alert("La oracion invertida es:" + nuevaCadena);
-  	alert("La letra e aparece " + (cadena.split("e").length - 1) + " veces.");
+	alert("La letra e aparece " + (cadena.split("e").length - 1) + " veces.");
 }
 
 invertirCadena(prompt("Introduce la frase que invertire."));
